4c4680cc17e6f3c9f7f363ade0a25e98059e95e0
Thiago Pereira <thiagorpereira7@gmail.com>
2020-05-26 15:47:37 -0300

    Sign In Sign Up

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,8 @@
+import { persistStore } from 'redux-persist';
 import createSagaMiddleware from 'redux-saga';
+
 import createStore from './createStore';
+import persistReducers from './persistReducers';
 
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
@@ -12,8 +15,10 @@ const sagaMiddleare = createSagaMiddleware({ sagaMonitor });
 
 const middlewares = [sagaMiddleare];
 
-const store = createStore(rootReducer, middlewares);
+const store = createStore(persistReducers(rootReducer), middlewares);
+
+const persistor = persistStore(store);
 
 sagaMiddleare.run(rootSaga);
 
-export default store;
+export { store, persistor };
